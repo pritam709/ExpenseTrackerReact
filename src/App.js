26971c1598dd3049555c4224df50a672e632362b
Pravesh <pritamyadav70923@gmail.com>
2023-04-28 16:11:16 +0530

    2023 added as option

--- src/App.js
+++ src/App.js
@@ -4,26 +4,7 @@
 import React, { useState } from "react";
 import NewExpenseForm from "./components/NewExpenseForm";
 import Expenses from "./components/Expenses";
-const dummyExpense = [
-  {
-    id: 1,
-    title: "pen",
-    amount: 20,
-    date: new Date("2020-04-18"),
-  },
-  {
-    id: 2,
-    title: "paper",
-    amount: 200,
-    date: new Date("2021-04-18"),
-  },
-  {
-    id: 3,
-    title: "notebook",
-    amount: 65,
-    date: new Date("2022-04-18"),
-  },
-];
+const dummyExpense = [];
 function App() {
   const [Expense, setExpense] = useState(dummyExpense);
 
